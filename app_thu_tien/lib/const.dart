310575b73df03719c9a6ce44af9f129afee5c3a5
PL Minh Phú <34944968+plminhphu@users.noreply.github.com>
2025-01-09 22:36:04 +0700

    clinet app

--- app_thu_tien/lib/const.dart
+++ app_thu_tien/lib/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 class Const {
-  static bool isAdmin = false;
+  static bool isAdmin = true;
   static BoxDecoration decoration = const BoxDecoration(
     color: CupertinoColors.systemBackground,
     borderRadius: BorderRadius.all(Radius.circular(7)),
